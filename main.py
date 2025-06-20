39b5f4313450e4adcfc7c728f9b0067b5e641c95
Homanti <vova0mb2@gmail.com>
2025-06-19 18:58:08 -0700

    lib fix

--- main.py
+++ main.py
@@ -302,6 +302,6 @@ async def upload_file(file: UploadFile = File(...)):
     return {"image_url": image_url}
 
 # -------------------- RUN (optional) --------------------
-if __name__ == "__main__":
-    import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+# if __name__ == "__main__":
+#     import uvicorn
+#     uvicorn.run(app, host="0.0.0.0", port=8000)
